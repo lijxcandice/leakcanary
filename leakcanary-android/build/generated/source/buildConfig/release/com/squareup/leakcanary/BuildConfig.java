b6cf56e4d9955a2c8034422e1466866182f432be
lijxcandice <lijxcandice@foxmail.com>
2016-05-17 17:51:06 +0800

    Initial commit

---
+++ leakcanary-android/build/generated/source/buildConfig/release/com/squareup/leakcanary/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.squareup.leakcanary;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.squareup.leakcanary";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = -1;
+  public static final String VERSION_NAME = "";
+  // Fields from default config.
+  public static final String GIT_SHA = "";
+  public static final String LIBRARY_VERSION = "1.4-SNAPSHOT";
+}
